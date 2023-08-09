23dfee95894be8d100713bcbd8506200aceedcea
Marcelo David Moisés <mache.uno@gmail.com>
2023-08-09 11:24:36 -0300

    Calendar implementation

--- api/status/index.ts
+++ api/status/index.ts
@@ -1,7 +1,8 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
+import { calendar } from "../../src/calendar";
 
 const handler = async (request: VercelRequest, response: VercelResponse) => {
-  return response.status(200).json({ status: "running" });
+  return response.status(200).json({ status: calendar.status });
 };
 
 export default handler;
